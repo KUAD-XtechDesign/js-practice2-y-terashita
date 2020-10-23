44fd67aefd7ae25da645e76036c900416ef62600
Y- terashita <y-terashita4278@st.kyoto-art.ac.jp>
2020-10-23 16:29:21 +0900

    練習問題

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -5,13 +5,16 @@ $(function(){
     function checkBox(){
         if(hako > 5){
             $("#content").text(hako+"個入っています。箱はいっぱいです。箱を空にします。");
+            /*$("#content").addClass('full');*/
             hako = 0;
 
         }else if(hako <= 0){
             hako = 0;
             $("#content").text(hako+"個。入っていません。");
+            /*$("#content").removeClass().addClass('empty')*/;
         }else{
             $("#content").text(hako+"個入っています。まだ入ります。");
+            /*$("#content").removeClass();*/
         }
     }
 
--- sample/js/script03.js
+++ sample/js/script03.js
@@ -1,5 +1,33 @@
 $(function(){
 
+    let hako = 0;
+
+    $("#button01").on("click",function(){
+        for(let i=0; i < 10; i++){
+            hako = hako + 2;
+        }
+        $("#content").text(hako);
+    })
+
+    $("#button02").on("click",function(){
+        for(let i=0; i < 10; i++){
+            hako = hako + i;
+        }
+        $("#content").text(hako);
+    })
+
+    $("#button03").on("click",function(){
+        setInterval(function(){
+            hako ++;
+            $("#content").text(hako);
+        },1000)
+    })
+
+    $("#button04").on("click",function(){
+        hako = 0;
+        $("#content").text(hako);
+    })
+
 
   
 })
