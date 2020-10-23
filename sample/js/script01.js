101c92427b35417d53f4b0791b9aa126be38577b
Y- terashita <y-terashita4278@st.kyoto-art.ac.jp>
2020-10-23 15:53:08 +0900

    練習問題

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,6 +1,47 @@
 $(function(){
 
+    let txt1 = "おはようございます！";
+    let txt2 = "お元気ですか？";
+    let num1 = 20;
+    let num2 = 32;
 
+    function sayHallo(){
+        /*何か処理*/
+        $("#content").text("こんにちは！");
+    }
 
+    function saySomething(txt){
+        $("#content").text(txt);
+    }
+
+    function samTwo(t1,t2){
+        $("#content").append(t1 + t2);
+    }
+
+    $("#button01").on("click",function(){
+        sayHallo();
+    })
+
+    $("#button02").on("click",function(){
+        saySomething(txt1);
+    })
+
+    $("#button03").on("click",function(){
+        /*$("#content").text(text1 + text2);*/
+        samTwo(txt1,txt2);
+    })
+
+    $("#button04").on("click",function(){
+        /*$("#content").text(num1 + num2);*/
+        samTwo(num1,num2);
+    })
+
+    $("#button05").on("click",function(){
+        /*num1 = num1 + 10
+         $("#ball").css("margin-left",num1 + 'px')
+        */
+        samTwo(txt1,txt2);
+        samTwo(num1,num2);
+    })
 
 })
--- sample/js/script02.js
+++ sample/js/script02.js
@@ -1,5 +1,32 @@
 $(function(){
 
+    let hako = 0;
 
+    function checkBox(){
+        if(hako > 5){
+            $("#content").text(hako+"個入っています。箱はいっぱいです。箱を空にします。");
+            hako = 0;
+
+        }else if(hako <= 0){
+            hako = 0;
+            $("#content").text(hako+"個。入っていません。");
+        }else{
+            $("#content").text(hako+"個入っています。まだ入ります。");
+        }
+    }
+
+    $("#button01").on("click",function(){
+            checkBox();
+    })
+
+    $("#button02").on("click",function(){
+            hako = hako + 1;
+            checkBox();
+    })
+
+    $("#button03").on("click",function(){
+            hako = hako - 1;
+            checkBox();
+    })
 
 })
